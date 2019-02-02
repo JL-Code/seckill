346b71537e013f78a4a1c99d23e807049ae7e39c
蒋勇 <jiangy@highzap.com>
2019-02-02 21:51:11 +0800

    * .gitignore: Init项目
    * SecKill.sln:
    * README.md:
    * Startup.cs:
    * Program.cs:
    * SecKill.csproj:
    * appsettings.json:
    * site.js:
    * favicon.ico:
    * site.css:
    * _ViewStart.cshtml:
    * Index.cshtml:
    * ErrorViewModel.cs:
    * _ViewImports.cshtml:
    * Privacy.cshtml:
    * Entity.cs:
    * Error.cshtml:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * LICENSE:
    * HomeController.cs:
    * LICENSE.txt:
    * launchSettings.json:
    * User.cs:
    * jquery.js:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * _CookieConsentPartial.cshtml:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.js.map:
    * bootstrap.min.js:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap-grid.css:
    * bootstrap.bundle.js:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * bootstrap.bundle.js.map:
    * bootstrap-grid.css.map:
    * bootstrap-grid.min.css:
    * jquery.validate.js:
    * LICENSE.txt:
    * bootstrap.bundle.min.js:
    * bootstrap-reboot.min.css:
    * bootstrap-reboot.css.map:
    * additional-methods.js:
    * jquery.validate.min.js:
    * bootstrap-grid.min.css.map:
    * bootstrap.bundle.min.js.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ SecKill/Domain/AggregatesModel/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SecKill.Domain.SeedWork;
+
+namespace SecKill.Domain.AggregatesModel
+{
+    public class User : Entity
+    {
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        /// <value>The user identifier.</value>
+        public Guid UserId { get; set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        /// <value>The name of the user.</value>
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+    }
+}
---
+++ SecKill/Domain/SeedWork/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SecKill.Domain.SeedWork
+{
+    /// <summary>
+    /// 实体抽象父类
+    /// </summary>
+    public abstract class Entity
+    {
+
+    }
+}
