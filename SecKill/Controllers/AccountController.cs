d06c92e39787f4e6b21c2c16d0c9ffbd1373f2ec
蒋勇 <jiangy@highzap.com>
2019-02-09 21:46:51 +0800

    添加登录模块

---
+++ SecKill/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SecKill.Infrastructure;
+using SecKill.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecKill.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        readonly DefaultContext _dbContext;
+
+        public AccountController(DefaultContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public IActionResult Login(AccountViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var users = _dbContext.Set<Domain.AggregatesModel.User>();
+            var user = users.Where(m => m.UserName == model.UserName && m.Password == model.Password).FirstOrDefault();
+
+            return Ok(user);
+        }
+    }
+}
---
+++ SecKill/Models/AccountViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SecKill.Models
+{
+    public class AccountViewModel
+    {
+        [Required(ErrorMessage ="用户名必填")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage ="密码必填")]
+        public string Password { get; set; }
+    }
+}
